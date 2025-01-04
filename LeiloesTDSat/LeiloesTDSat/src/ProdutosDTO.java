40df79ee6fa6c78c34b619d460452fde516a0b3f
Allan Victor <allanvictor13777@gmail.com>
2025-01-04 13:57:12 -0300

    (sessão de hiperfoco) Implementada a função ListarProdutosVendidos, adicionada tela Vendas, implementado o botão atualizar da tela Vendas e Implementada a funcionalidade do botão Consultar Vendas

--- LeiloesTDSat/LeiloesTDSat/src/ProdutosDTO.java
+++ LeiloesTDSat/LeiloesTDSat/src/ProdutosDTO.java
@@ -44,5 +44,16 @@ public class ProdutosDTO {
     public void setStatus(String status) {
         this.status = status;
     }
+
+    public ProdutosDTO() {}
+    
+    public ProdutosDTO(Integer id, String nome, Integer valor, String status) {
+        this.id = id;
+        this.nome = nome;
+        this.valor = valor;
+        this.status = status;
+    }
+    
+    
     
 }
